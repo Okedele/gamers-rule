77f5298912db1293adb32c92f12ef5dd04d59243
Chibueze-20 <analogbeichibueze@gmail.com>
2019-01-27 22:06:48 +0100

    documentation

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ let usercontroller = require("./Controllers/user.controller");
 app.get('/',function(req,res){
     res.sendFile("/index.html",{root : __dirname+"/app"});
 })
+app.get('/created',function(req,res){
+  let data = [
+    {name:"garaga",date:"1/1/12",players:3},
+    {name:"meetings_of_gods",date:"1/12/12",players:4},
+    {name:"DnD_Raw",date:"1/8/14",players:5},
+    {name:"garAgA",date:"1/9/15",players:6}
+]
+  res.send(data)
+})
+app.get('/joined',function(req,res){
+  let data =[
+    {name:"REgaraga",date:"1/10/12",playing:false},
+    {name:"class_19_fantasy",date:"1/5/13",playing:false},
+    {name:"hour_of_recon",date:"1/3/15",playing:false},
+    {name:"LArange",date:"1/5/18",playing:true}
+  ]
+  res.send(data)
+})
 app.use('/user',usercontroller);
 
 http.createServer(app).listen(port);
